65d4b8da47635ffefa5f61dad6607d9e410805d9
Nick Fishbane <nicholas.fishbane@stat.ubc.ca>
2014-09-05 14:54:15 -0700

    Revert "fishyTools package"
    This reverts commit 5855ab0ee6b83c659c2fd5433fd48debd2e95b40.

--- R/my.gc.R
+++
@@ -1,9 +0,0 @@
-#' Collect all garbage, i.e. clear temporary memory. Keeps running /code{gc()} until output
-#'  doesn't change
-#' @export
-  
-my.gc <- function () 
-{
-    while (gc()[2, 4] != gc()[2, 4] | gc()[1, 4] != gc()[1, 4]) {
-    }
-}
